a8cdc3c6e82c46ccf3c8dfe3e804054eb61f47cc
Jon Higgs <jhiggs@eml.cc>
2013-09-07 18:31:41 +0200

    make aliases/ require logged in user.

--- app/controllers/aliases_controller.rb
+++ app/controllers/aliases_controller.rb
@@ -1,6 +1,17 @@
 class AliasesController < ApplicationController
   def index
     redirect_to("/users/sign_in") unless user_signed_in?
+  end
 
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
   end
 end
