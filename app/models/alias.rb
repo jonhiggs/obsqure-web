7da0e1d6485506cce2ca63c9db7fad38bd2d1f51
Jon Higgs <jhiggs@eml.cc>
2013-09-08 19:36:43 +0200

    attempt to find a free address if we have a clash.

--- app/models/alias.rb
+++ app/models/alias.rb
@@ -11,9 +11,17 @@ class Alias < ActiveRecord::Base
 
 private
   def generate_address
-    length = 8
-    chars = (0..9).to_a + ("A".."Z").to_a
-    name = length.times.map{ (chars[rand(chars.size)].to_s) }.join
-    "#{name}@obsqure.me"
+    address = nil
+    attempt = 0
+    while Alias.find_by_address(address) || address.nil?
+      attempt += 1
+      length = 6
+      chars = (0..9).to_a + ("A".."Z").to_a
+      name = length.times.map{ (chars[rand(chars.size)].to_s) }.join
+      address = "#{name}@obsqure.me"
+      puts "attempt number: #{attempt}".inspect
+      raise "alias namespace is getting exhausted" if attempt > 20
+    end
+    address
   end
 end
