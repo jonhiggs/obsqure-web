b785353979f52bd0bdf7ad4de8f360996b3a027b
Jon Higgs <jhiggs@eml.cc>
2013-09-08 18:13:35 +0200

    populate the redirect_to field for aliases.

--- app/models/alias.rb
+++ app/models/alias.rb
@@ -1,11 +1,19 @@
 class Alias < ActiveRecord::Base
   belongs_to :user
-  before_save :generate_address
+  before_save :redirect_to
+  before_save :save_defaults
 
+
+  def save_defaults
+    self.redirect_to ||= self.user.email
+    self.address ||= generate_address
+  end
+
+private
   def generate_address
     length = 8
     chars = (0..9).to_a + ("A".."Z").to_a
     name = length.times.map{ (chars[rand(chars.size)].to_s) }.join
-    self.address ||= "#{name}@obsqure.me"
+    "#{name}@obsqure.me"
   end
 end
