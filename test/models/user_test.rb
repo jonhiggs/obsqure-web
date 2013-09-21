c42422730740f691349e8cdf1261225cba1db73e
Jon Higgs <jhiggs@eml.cc>
2013-09-21 10:17:31 +0200

    start building some tests.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
+  test "test a user" do
+    user = User.find_by_email("addr1@domain.com")
+    assert user.id == 1
+  end
+
 end
