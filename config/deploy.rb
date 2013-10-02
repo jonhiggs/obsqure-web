a1f0f969d88b9a34674a937ad80236a981b08bc4
Jon Higgs <jhiggs@eml.cc>
2013-10-02 18:18:25 +0700

    more fixes to the cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,14 @@
 set :application, "obsqure"
-set :repository,  "git.obsqure.net:/srv/git/repos/obsqure.git"
+set :repository,  "git@git.obsqure.net:/srv/git/repos/obsqure.git"
 set :scm, :git
 set :user, "www-data"
 set :use_sudo, false
-set :scm_username, "git"
 
 set :deploy_to, "/srv/www/obsqure"
 
 role :web, "www.obsqure.net"                          # Your HTTP server, Apache/etc
 role :app, "www.obsqure.net"                          # This may be the same as your `Web` server
-role :db,  "db1.obsqure.net", :primary => true        # This is where Rails migrations will run
+role :db,  "www.obsqure.net", :primary => true        # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
