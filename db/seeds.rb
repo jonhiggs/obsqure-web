de89dd3d2a7eddfc48f475c5221867996214287e
Jon Higgs <jhiggs@eml.cc>
2013-10-06 01:03:06 +0700

    add extra default users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
-u = User.new(
-  :username => "admin",
-  :password => "adminadmin"
-)
-u.save!(:validate => false)
+default_users = [
+  { :username => "admin", :password => "xi5xe6uj" },
+  { :username => "administrator", :password => "or1poh9o" },
+  { :username => "ftp", :password => "vootaht6" },
+  { :username => "guest", :password => "dai2dahd" },
+  { :username => "jon", :password => "po5ay1ve" },
+  { :username => "postmaster", :password => "moba0ahw" },
+  { :username => "root", :password => "taethoo6" },
+  { :username => "support", :password => "ien2iph2" }
+]
+
+default_users.each do |user|
+  u = User.new(user)
+  u.save!(:validate => false)
+end
