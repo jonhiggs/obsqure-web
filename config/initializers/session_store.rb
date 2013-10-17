c93d72446b851f806ec7b75287d6e0837b0c7fc7
Jon Higgs <jhiggs@eml.cc>
2013-10-17 17:50:26 +0700

    expire session after being idle for 10 minutes.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Obsqure::Application.config.session_store :cookie_store, key: '_obsqure_session'
+Obsqure::Application.config.session_store :cookie_store,
+  key: '_obsqure_session',
+  expire_after: 10.minutes
